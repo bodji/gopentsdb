29ce8a6ed03313b557f6eb0f02a3ba88cb922623
Charles-Antoine Mathieu <skatkatt@root.gg>
2015-03-01 22:05:52 +0100

    async and multi connections

--- gopentsdb.go
+++ gopentsdb.go
@@ -11,19 +11,148 @@ import (
 	"time"
 	"sort"
 	"crypto/tls"
+	"strconv"
 )
 
-var opentsdbConnection net.Conn
-var opentsdbWriteLock *sync.RWMutex
+var verbose bool;
+
+type Tsd struct {
+	host 	string
+	port    int
+	ssl		bool
+
+	puts 		chan *Put
+	conn 	   	net.Conn
+	connected  	bool
+	lock	   	sync.Mutex
+}
+
+func NewTsd(strconn string, ssl bool, puts chan *Put) (this *Tsd, err error){
+	this = new(Tsd)
+
+	items := strings.Split(strconn,":")
+	switch len(items){
+	case 1:
+		this.host = items[0]
+		this.port = 4242
+	case 2:
+		this.host = items[0]
+		if this.port, err = strconv.Atoi(items[1]); err != nil{
+			return nil, err
+		}
+	default:
+		err = errors.New("gopentsdb: Invalid tsd address " + strconn)
+		return nil, err
+	}
+	this.ssl = ssl
+
+	go func() {
+		for {
+			if !this.StillAlive() {
+				if err := this.Connect() ; err == nil {
+					log.Printf("gopentsdb: Connected to %s:%d\n",this.host,this.port)
+				} else {
+					log.Printf("gopentsdb: Connection failed to %s:%d : %s\n",this.host,this.port,err)
+				}
+			}
+			time.Sleep(time.Duration(10) * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			if this.connected {
+				this.Put(<- puts)
+			} else {
+				time.Sleep(time.Second)
+			}
+		}
+	}()
+
+	return
+}
+
+func (this *Tsd) Connect() (err error) {
+	if this.conn, err = net.DialTimeout("tcp", fmt.Sprintf("%s:%d", this.host, this.port), time.Second*2) ; err != nil {
+		return
+	}
+	if this.ssl {
+		var tlsconn *tls.Conn
+		tlsconn = tls.Client(this.conn,&tls.Config{ InsecureSkipVerify: true })
+		if err = tlsconn.Handshake() ; err != nil {
+			return
+		}
+		this.conn = tlsconn
+	}
+	this.connected = true
+	return
+}
+
+func (this *Tsd) StillAlive() bool {
+	// Test nil
+	if this.conn == nil {
+		return false
+	}
+
+	// Lock before write/read socket
+	this.lock.Lock()
+	this.lock.Unlock()
+
+	// Send "version" to socket
+	fmt.Fprintf(this.conn, "version"+"\n")
+
+	// Get content
+	this.conn.SetReadDeadline(time.Now().Add(time.Second))
+	reply := new(bytes.Buffer)
+
+	for {
+		buf := make([]byte, 512)
+		read_len, err := this.conn.Read(buf)
+		if err != nil {
+			break
+		}
+		reply.Write(buf[:read_len])
+	}
+
+	// Test response
+	if strings.Contains(reply.String(), "net.opentsdb") {
+		return true
+	}
+
+	log.Printf("gopentsdb: Connection lost to %s:%d\n",this.host,this.port)
+	this.connected = false
+	this.conn.Close()
+	return false
+}
+
+func (this *Tsd) Put(p *Put) (i int, err error) {
+	// Lock
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	// Are we still connected to OpenTSDB ?
+	if !this.connected {
+		return -1, errors.New("gopentsdb: Can't put, not connected")
+	}
+
+	// Put
+	i, err = fmt.Fprintf(this.conn, p.ToString()+"\n")
+	if err != nil {
+		return i, err
+	}
+
+	// Log?
+	if verbose {
+		log.Printf("gopentsdb: " + p.ToString() + "\n")
+	}
+
+	return i, err
+}
 
 type OpenTsdb struct {
-	TsdAddress string
-	TsdPort    int
-
-	connected bool
-	verbose   bool
-
-	deduplication		bool
+	tsds	  			[]*Tsd
+	puts				chan *Put
+	deduplication		int
 	deduplicationMap  	map[string]*Put
 }
 
@@ -33,58 +162,28 @@ type OpenTsdb struct {
 // - deduplication is a boolean to enable deduplication of puts in a 10 minutes range
 //   (that means that if a put has the same value, tags, and metric, on a 10 minute period, it will be pushed once to OpenTSDB)
 // - ssl enable ssl on tsd socket. Certificate is not verified
-func NewOpenTsdb(address string, port int, verbose bool, deduplication bool, ssl bool) (this *OpenTsdb) {
-
+func NewOpenTsdb(hosts []string, ssl bool, deduplication int, buffersize int) (this *OpenTsdb, err error) {
 	this = new(OpenTsdb)
-	this.TsdAddress = address
-	this.TsdPort = port
-	this.connected = false
-	this.verbose = verbose
+
+	this.puts = make(chan *Put, buffersize)
+
+	this.tsds = make([]*Tsd,len(hosts))
+	for i, host := range hosts {
+		if this.tsds[i], err = NewTsd(host,ssl,this.puts); err != nil {
+			return nil, err
+		}
+	}
 
 	this.deduplication = deduplication
-	this.deduplicationMap = make(map[string]*Put)
-
-	// Init mutex
-	opentsdbWriteLock = new(sync.RWMutex)
-
-	// Launch goroutine to periodically check
-	go func() {
-		for {
-			if !this.StillAlive() {
-
-				// Connection to OpenTSDB
-				var connection net.Conn
-				var err	error
-
-				if ssl {
-					connection, err = tls.Dial("tcp", fmt.Sprintf("%s:%d", this.TsdAddress, this.TsdPort), &tls.Config{ InsecureSkipVerify: true })
-				} else {
-					connection, err = net.DialTimeout("tcp", fmt.Sprintf("%s:%d", this.TsdAddress, this.TsdPort), time.Second*2)
-				}
-
-				if err != nil {
-					this.connected = false
-					log.Printf("Failed to connect to OpenTSDB : %s\n", err)
-				} else {
-                    if !this.connected {
-					    this.connected = true
-					    log.Println("Connected to OpenTSDB")
-					    opentsdbConnection = connection
-                    }
-				}
-			}
-
-			time.Sleep(time.Second)
-		}
-	}()
 
 	// Launch goroutine to clean dedup map
-	if deduplication {
+	if deduplication > 0 {
+		this.deduplicationMap = make(map[string]*Put)
 		go func() {
 			for {
 				currentTimestamp := time.Now().Unix()
 				for putFootPrint := range this.deduplicationMap {
-					if currentTimestamp - this.deduplicationMap[putFootPrint].timestamp > 600 {
+					if currentTimestamp - this.deduplicationMap[putFootPrint].timestamp > int64(deduplication) {
 						delete(this.deduplicationMap, putFootPrint)
 					}
 				}
@@ -96,41 +195,24 @@ func NewOpenTsdb(address string, port int, verbose bool, deduplication bool, ssl
 	return
 }
 
-func (this *OpenTsdb) Put(p *Put) (i int, err error) {
-
-	// Are we connected to OpenTSDB ?
-	if !this.connected {
-		return -1, errors.New("gopentsdb: Can't put, not connected")
-	}
-
+func (this *OpenTsdb) Put(put *Put) {
 	// Duplicate ?
-	if this.deduplication && this.IsDuplicate( p ) {
-		if this.verbose {
-			log.Printf("[GOPENTSDB] Duplicate : " + p.ToString() + "\n")
+	if this.deduplication > 0 && this.IsDuplicate( put ) {
+		if verbose {
+			log.Printf("gopentsdb: discarding " + put.ToString() + " : duplicate\n")
 		}
-		return 0,nil
 	}
 
-	// Lock
-	opentsdbWriteLock.Lock()
-	defer opentsdbWriteLock.Unlock()
-
-	// Put
-	i, err = fmt.Fprintf(opentsdbConnection, p.ToString()+"\n")
-	if err != nil {
-		return i, err
+	select {
+	case this.puts <- put:
+	default:
+		if verbose {
+			log.Printf("gopentsdb: discarding " + put.ToString() + " : channel is full\n")
+		}
 	}
-
-	// Log?
-	if this.verbose {
-		log.Printf("[GOPENTSDB] " + p.ToString() + "\n")
-	}
-
-	return i, err
 }
 
 func (this *OpenTsdb) IsDuplicate(p *Put) ( duplicate bool ) {
-
 	duplicate = false
 
 	// Sort tags
@@ -172,37 +254,6 @@ func (this *OpenTsdb) IsDuplicate(p *Put) ( duplicate bool ) {
 	return duplicate
 }
 
-func (this *OpenTsdb) StillAlive() bool {
-
-	// Test nil
-	if opentsdbConnection == nil {
-		return false
-	}
-
-	// Lock before write/read socket
-	opentsdbWriteLock.Lock()
-	defer opentsdbWriteLock.Unlock()
-
-	// Send "version" to socket
-	fmt.Fprintf(opentsdbConnection, "version"+"\n")
-
-	// Get content
-	opentsdbConnection.SetReadDeadline(time.Now().Add(time.Second))
-	completeOutput := new(bytes.Buffer)
-
-	for {
-		reply := make([]byte, 512)
-		read_len, err := opentsdbConnection.Read(reply)
-		if err != nil {
-			break
-		}
-		completeOutput.Write(reply[:read_len])
-	}
-
-	// Test response
-	if strings.Contains(completeOutput.String(), "net.opentsdb") {
-		return true
-	}
-
-	return false
+func Verbose(enabled bool){
+	verbose = enabled
 }
--- gopentsdb_test.go
+++ gopentsdb_test.go
@@ -3,6 +3,7 @@ package gopentsdb
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func Test_NewPut(t *testing.T) {
@@ -14,5 +15,23 @@ func Test_NewPut(t *testing.T) {
 
 	put := NewPut("hardware_load_average", tags, 1.05)
 	fmt.Println(put.ToString())
+}
 
+func Test_Functional(t *testing.T) {
+	Verbose(true)
+	if tsdb, err := NewOpenTsdb([]string{"127.0.0.1","127.0.0.1"},false,0,10) ; err == nil {
+		for i:=0 ; i<1000 ; i++ {
+			// Tags
+			tags := make(map[string]string)
+			tags["load"] = "load15"
+			tags["hostname"] = "localhost"
+
+			put := NewPut("hardware_load_average", tags, 1.05)
+			fmt.Println(put.ToString())
+			tsdb.Put(put)
+			time.Sleep(time.Duration(100)*time.Millisecond)
+		}
+	} else {
+		fmt.Println(err)
+	}
 }
